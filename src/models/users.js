22ad8daa46b1447a5a00e627f755c1dba0a2bdd2
ASHLEY BREUNICH <ashleybreunich@gmail.com>
2018-10-27 17:34:00 -0700

    submitting new test

--- src/models/users.js
+++ src/models/users.js
@@ -1,7 +1,6 @@
 'use strict';
 
 import storage from '../lib/storage/storage.js';
-// var revalidator = require('revalidator');
 
 class Users {
 
@@ -34,10 +33,12 @@ class Users {
   static put(id, data) {
     return storage.save(data);
   }
+
   static patch(id, data) {
     data._id = id;
     return storage.save(data);
   }
+  
 }
 
 export default Users;
